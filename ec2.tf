f5c5a6ca75de7361c0169bdc1c95d42121008795
EC2 Default User <ec2-user@ip-172-30-1-51.ap-northeast-1.compute.internal>
2022-09-28 12:38:10 +0000

    github actions terraform test 5

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,8 @@
-resource "aws_instance" "example" {
-  ami           = ami-078296f82eb463377
-  instance_type = "t3.micro"
+data "aws_ssm_parameter" "amzn2_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "demo" {
+  ami           = data.aws_ssm_parameter.amzn2_ami.value
+  instance_type = "t2.micro"
 }
